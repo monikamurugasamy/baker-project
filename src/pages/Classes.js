131c5a4608c21514797c8fbf3f0c5633b108132d
Monika <monikamurugasamy@gmail.com>
2022-09-15 16:57:13 +0530

    login page

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -5,7 +5,7 @@ const classes = () => {
         <>
         <div className='image-1'>
             <div className='img'>
-         <h1 className='off'><b>Offline classes</b> </h1>
+         <h1 className='off'><b>Offline classes</b> </h1> 
          <p className='offline1'>We have taken our popular Baking and Decorating course and created an Offline version. Over the course of these 5 modules, you will learn how to make basic sugar flowers, chocolate ganache, Swiss Meringue Buttercream, create sharp edges and create a stunning 2 tick wedding cake and a drip cake.</p>
         </div>
         </div>
